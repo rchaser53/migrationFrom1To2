02cb5da235daf9f13afd10b34eb48b30f92f0fe0
rchaser53 <tayoshizawa29@gmail.com>
2017-07-05 22:30:54 +0900

    implement vue 1 fade

--- src/index.js
+++ src/index.js
@@ -2,8 +2,30 @@ const Vue = require('vue')
 
 var app = new Vue({
   el: '#app',
-  template: '<p>{{seen}}</p>',
+  template: `
+    <div>
+      <button v-on:click='switch'>switch</button>
+      <div v-if='flag' transition="fade">
+        <p>hoge-</p>
+      </div>
+    </div>
+  `,
   data: {
-    seen: true
+    flag: true
+  },
+  methods: {
+    switch: function() {
+      console.log(1234)
+      this.flag = !this.flag
+    }
+  }
+})
+
+Vue.transition('fade', {
+  afterEnter: function(element, done) {
+    console.log('after')
+  },
+  leave: function(element, done) {
+    console.log('leave')
   }
 })
