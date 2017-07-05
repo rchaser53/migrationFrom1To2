0aa4b7287b14c2f666f67c60886ea930710ffe50
rchaser53 <tayoshizawa29@gmail.com>
2017-07-05 23:21:03 +0900

    try to use method in template

--- src/index.js
+++ src/index.js
@@ -5,19 +5,23 @@ var app = new Vue({
   template: `
     <div>
       <button v-on:click='toggle'>toggle</button>
-      
       <transition v-if='flag' name="fade">
-        <p>hoge-</p>
+        <p v-bind:class="multiple(numA, numB)">hoge-</p>
       </transition>
     </div>
   `,
   data: {
-    flag: true
+    flag: true,
+    numA: 11,
+    numB: 12
   },
   methods: {
     toggle: function() {
       console.log(1234)
       this.flag = !this.flag
+    },
+    multiple: function(numA, numB) {
+      return `class${numA * numB}`
     }
   }
 })
